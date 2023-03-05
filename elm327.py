5dab01717991d87194d5ae492169f3e104c94717
mikolajwojciuk <56088389+mikolajwojciuk@users.noreply.github.com>
2023-03-05 23:17:53 +0100

    'gitignore'

--- elm327.py
+++ elm327.py
@@ -1,5 +1,5 @@
 from time import sleep
-from utils import decode_message
+from utils import *
 from constant import EOM_default
 
 
@@ -18,71 +18,71 @@ class ELM327:
         # Each entry is in the format:
         # (mode, pid): lines
         self.num_lines = {
-            ('01', '05'): None, # engine coolant temperature
-            ('01', '0B'): None, # intake manifold pressure
-            ('01', '0C'): None, # engine rpm
-            ('01', '0D'): None, # vehicle speed
-            ('01', '11'): None, # throttle position
-            ('01', '5C'): None, # engine oil temperature
-            ('09', '02'): None, # VIN
+            ("01", "05"): 0,  # engine coolant temperature
+            ("01", "0B"): 0,  # intake manifold pressure
+            ("01", "0C"): 0,  # engine rpm
+            ("01", "0D"): 0,  # vehicle speed
+            ("01", "11"): 0,  # throttle position
+            ("01", "5C"): 0,  # engine oil temperature
+            ("09", "02"): 0,  # VIN
         }
 
     def reset(self):
         """Simple test to make the LEDs on the ELM327 flash in sequence."""
-        return self.AT('Z')
+        return self.AT("Z")
 
     def read_battery_voltage(self):
         """Read the voltage at the car's battery."""
-        return self.AT('RV')
+        return self.AT("RV")
 
     def search_protocol(self):
-        return self.set_protocol('0')
+        return self.set_protocol("0")
 
     def set_protocol(self, protocol):
-        return self.AT(f'SP {protocol}')
+        return self.AT(f"SP {protocol}")
 
     def get_engine_coolant_temperature(self):
-        ret = self.obd_get_current_data('05')
+        ret = self.obd_get_current_data("05")
         return decode_temperature(ret[0])
 
     def get_intake_manifold_pressure(self):
-        ret = self.obd_get_current_data('0B')
+        ret = self.obd_get_current_data("0B")
         return decode_pressure(ret[0])
 
     def get_engine_rpm(self):
-        ret = self.obd_get_current_data('0C')
+        ret = self.obd_get_current_data("0C")
         return decode_rpm(ret[0])
 
     def get_speed(self):
-        ret = self.obd_get_current_data('0D')
+        ret = self.obd_get_current_data("0D")
         return decode_speed(ret[0])
 
     def get_engine_oil_temperature(self):
-        ret = self.obd_get_current_data('5C')
+        ret = self.obd_get_current_data("5C")
         return decode_temperature(ret[0])
 
     def get_vin(self):
-        ret = self.obd_get_vehicle_information('02')
+        ret = self.obd_get_vehicle_information("02")
         # TODO: parse return
         return ret
 
     def AT(self, atcmd):
-        return self.query('AT' + atcmd)
+        return self.query("AT" + atcmd)
 
     def OBD(self, mode, pid):
         OBD_MODES = [
-            '01', # show current data
-            '02', # show freeze frame data
-            '03', # show diagnostics trouble codes
-            '04', # clear trouble codes and stored values
-            '05', # test results, oxygen sensors
-            '06', # test results, non-continuously monitored
-            '07', # show 'pending' trouble codes
-            '08', # special control mode
-            '09', # request vehicle information
-            '0A'  # request permanent trouble codes
+            "01",  # show current data
+            "02",  # show freeze frame data
+            "03",  # show diagnostics trouble codes
+            "04",  # clear trouble codes and stored values
+            "05",  # test results, oxygen sensors
+            "06",  # test results, non-continuously monitored
+            "07",  # show 'pending' trouble codes
+            "08",  # special control mode
+            "09",  # request vehicle information
+            "0A",  # request permanent trouble codes
         ]
-        assert mode in OBD_MODES, f'invalid mode: {mode}'
+        assert mode in OBD_MODES, f"invalid mode: {mode}"
 
         # check how many lines to expect for the response
         # (will be None if first time using the command)
@@ -90,13 +90,13 @@ class ELM327:
 
         # first byte is mode
         # 2nd and 3rd bytes are parameter identification (PID)
-        cmd = f'{mode} {pid}'
+        cmd = f"{mode} {pid}"
         if N:
-            cmd += f' {N}'
+            cmd += f" {N}"
 
         ret = self.query(cmd)
 
-        lines = ret.split('\r')
+        lines = ret.split("\r")
 
         # remove echo (first line is always command sent)
         lines = lines[1:]
@@ -113,13 +113,13 @@ class ELM327:
             try:
                 l = []
                 # remove whitespace
-                line = line.replace('\r', '').replace('>', '').replace(' ', '')
+                line = line.replace("\r", "").replace(">", "").replace(" ", "")
                 # test if UNABLETOCONNECT
-                if line == 'UNABLETOCONNECT':
-                    raise Exception('Car not connected')
+                if line == "UNABLETOCONNECT":
+                    raise Exception("Car not connected")
                 # read ever set of 2 ascii chars as an 8-bit hex number
                 for i in range(0, len(line), 2):
-                    l += [int(line[i:i + 2], 16)]
+                    l += [int(line[i : i + 2], 16)]
 
                 ret += [bytes(l)]
             except ValueError:
@@ -128,36 +128,36 @@ class ELM327:
         return ret
 
     def obd_get_current_data(self, pid):
-        return self.OBD('01', pid)
+        return self.OBD("01", pid)
 
     def obd_get_vehicle_information(self, pid):
-        return self.OBD('09', pid)
+        return self.OBD("09", pid)
 
     def query(self, cmd):
         self.send(cmd)
         return self.read_until(self.EOM)
 
     def send(self, cmd):
-        self.write(cmd + '\r')
+        self.write(cmd + "\r")
 
     def write(self, cmd):
         self.uart.write(cmd.encode())
 
     def read_until(self, EOM):
-        ret = ''
+        ret = ""
         n = len(EOM)
 
         while ret[-n:] != EOM:
             while self.uart.any() == 0:
                 sleep(0.05)
             d = self.uart.read()
-            
+
             try:
                 ret = decode_message(d)
                 print(ret)
-                #ret += decode_message(d)
-                
+                # ret += decode_message(d)
+
             except UnicodeError:
                 print("uart decode error")
-        
+
         return ret
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from machine import Pin, UART
 from time import sleep
-from utils import decode_message
 from elm327 import ELM327
 
 
@@ -11,40 +10,28 @@ led.value(0)
 # init UART (connector is GND TX RX VDD)
 serial = UART(0, baudrate=38400, tx=Pin(0), rx=Pin(1))
 
-# cmd = 'ATI\r'
-# serial.write(cmd)
-# 
-# while True:
-#     if serial.any():
-#         data = serial.read()
-#         print(data)
-#         print(decode_message(data))
-
-
 # init ELM327 (OBD reader)
-print('Resetting ELM327...')
+print("Resetting ELM327...")
 elm = ELM327(serial)
 elm.reset()
-print('ELM reset done!')
+print("ELM reset done!")
+
+voltage = None
 
 
 while True:
     # toggle led for good measure (crash indicator)
     led.toggle()
-    sleep(0.5)
-    
+    sleep(1)
+
     try:
-        #speed = int(elm.get_speed())
-        #rpm = int(elm.get_engine_rpm())
-        #pressure = int(elm.get_intake_manifold_pressure())
+        # speed = int(elm.get_speed())
+        # rpm = int(elm.get_engine_rpm())
+        # pressure = int(elm.get_intake_manifold_pressure())
         voltage = elm.read_battery_voltage()
-        print(voltage)
-        
-        
-        
-       
+
     except Exception:
         print("Data not recieved!")
         sleep(0.5)
 
-
+    print("Voltage:         ", voltage)
